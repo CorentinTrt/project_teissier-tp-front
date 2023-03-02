821cd4d72d15bd69d3e052b8051119f56490aba8
CorentinTrt <corentintruffaut@gmail.com>
2023-03-02 09:55:20 -0500

    pages/home - import catch / xpList component + handle props

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,23 @@
-import Head from 'next/head';
 import s from './home.module.scss';
 
-const HomePage = () => {
-	return (
-		<div className={s['home']}>
-			<Metas />
+import { Strings, Upload } from '@c_types/T_pageData';
+import Catch from '@blocs/Catch';
+import XpList from '@blocs/XpList';
 
-			<div className={s['home--content']}>
-				<h1 className={s['title']}>Next.Js - Starter</h1>
-			</div>
-		</div>
-	);
+type Props = {
+	strings: Strings;
+	uploads: Upload[];
 };
 
-const Metas = () => {
+const HomePage = (props: Props) => {
+	const { strings, uploads } = props;
+
 	return (
-		<Head>
-			<title>Starter Next.Js - Home</title>
-			<meta name='description' content='Home page of the Next.Js starter' />
-		</Head>
+		<div className={s['home']}>
+			<Catch strings={strings} imgData={uploads[0]} />
+
+			<XpList strings={strings} />
+		</div>
 	);
 };
 
