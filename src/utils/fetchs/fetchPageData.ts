95ebb89e5ce3345841b8a3db308ce54f2809f212
CorentinTrt <corentintruffaut@gmail.com>
2023-03-02 09:56:33 -0500

    utils/fetchs/fetchPageData - create helper

---
+++ src/utils/fetchs/fetchPageData.ts
@@ -0,0 +1,13 @@
+type Page = 'home' | 'realisations';
+
+const strapiHost = process.env.STRAPI_HOST;
+const strapiPort = process.env.STRAPI_PORT;
+
+export default async function fetchPageData(page: Page) {
+	const result = await fetch(
+		`http://${strapiHost}:${strapiPort}/api/page-${page}?populate=*`
+	);
+	const json = await result.json();
+
+	return json;
+}
