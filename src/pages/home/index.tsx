d682b15843cfc4c03beae1a8a4c039691c220ed0
CorentinTrt <corentintruffaut@gmail.com>
2023-03-08 15:02:15 -0500

    page/home - add realisations

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,20 +3,25 @@ import s from './home.module.scss';
 import { Strings, Upload } from '@c_types/T_pageData';
 import Catch from '@blocs/Catch';
 import XpList from '@blocs/XpList';
+import Realisations from '@blocs/Realisations';
 
 type Props = {
 	strings: Strings;
 	uploads: Upload[];
+	realisations: Upload[];
 };
 
 const HomePage = (props: Props) => {
-	const { strings, uploads } = props;
+	const { strings, uploads, realisations } = props;
+	console.log(realisations);
 
 	return (
 		<div className={s['home']}>
 			<Catch strings={strings} imgData={uploads[0]} />
 
 			<XpList strings={strings} />
+
+			<Realisations strings={strings} realList={realisations} />
 		</div>
 	);
 };
