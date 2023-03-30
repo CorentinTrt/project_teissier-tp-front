9cf7e6d25b8726b8906ce9c73e793b096ce6def1
CorentinTrt <corentintruffaut@gmail.com>
2023-03-30 07:19:27 -0400

    4_shared/ActiveLink - create component

---
+++ src/components/4_shared/ActiveLink/index.tsx
@@ -0,0 +1,30 @@
+import s from './ActiveLink.module.scss';
+
+import { useRouter } from 'next/router';
+
+import { MouseEvent, ReactNode } from 'react';
+
+type Props = { href: string; children: ReactNode };
+
+const defaultProps: Props = { href: '/', children: <></> };
+
+const ActiveLink = (props: Props) => {
+	const { href, children } = props;
+
+	const router = useRouter();
+
+	function handleClick(e: MouseEvent) {
+		router.push(href);
+		e.preventDefault();
+	}
+
+	return (
+		<a className={s['active-link']} onClick={handleClick}>
+			{children}
+		</a>
+	);
+};
+
+export default ActiveLink;
+
+ActiveLink.defaultProps = defaultProps;
