e71587ad95807d6677bc19c80921c72e4084242c
CorentinTrt <corentintruffaut@gmail.com>
2023-03-30 07:18:33 -0400

    3_blocs/RealLayout - create component

---
+++ src/components/3_blocs/RealLayout/index.tsx
@@ -0,0 +1,51 @@
+import s from './realLayout.module.scss';
+
+import { Layout } from '@c_types/T_realisation';
+import { Upload } from '@c_types/T_generics';
+
+type Props = { layout: Layout; images: Upload[] };
+
+type LayoutData = {
+	classSuffix: 'un' | 'deux' | 'trois' | 'quatre';
+	imgCount: 1 | 3 | 4;
+};
+
+const defaultProps: Props = { layout: 1, images: [] };
+
+const RealLayout = (props: Props) => {
+	const { layout, images } = props;
+
+	const layoutData: LayoutData =
+		layout === 1
+			? { classSuffix: 'un', imgCount: 1 }
+			: layout === 2
+			? { classSuffix: 'deux', imgCount: 3 }
+			: layout === 3
+			? { classSuffix: 'trois', imgCount: 3 }
+			: layout === 4
+			? { classSuffix: 'quatre', imgCount: 4 }
+			: { classSuffix: 'un', imgCount: 1 };
+
+	const imgs: Upload[] = [];
+
+	for (let i = 0; i < layoutData.imgCount; i++) {
+		imgs.push(images[i]);
+	}
+
+	return (
+		<div className={`${s['real-layout']} ${s[layoutData.classSuffix]}`}>
+			{imgs.map((e, i) => (
+				<div
+					className={`${s['images']} ${s[`grid-item${i + 1}`]}`}
+					key={`img-real-${i}`}
+				>
+					<img src={e.url} alt={e.alternativeText} />
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default RealLayout;
+
+RealLayout.defaultProps = defaultProps;
