83ce9c9c75ccf4e156d3fb15409ed4b8db9c5f6c
CorentinTrt <corentintruffaut@gmail.com>
2023-03-30 07:22:44 -0400

    types/T_realisation - create types

---
+++ src/types/T_realisation.ts
@@ -0,0 +1,34 @@
+import type { Upload, UploadFromStrapi } from '@c_types/T_generics';
+
+// layouts
+// 1 > 1 image
+// 2 > 3 images > big at left
+// 3 > 3 images > big at the bottom
+// 4 > 4 images > same size, grid
+type Layout = 1 | 2 | 3 | 4;
+
+type Realisation = {
+	pid: number;
+	layout: Layout;
+	name: string;
+	typeText: string;
+	technicText: string;
+	images: Upload[];
+};
+
+type StrapiPayload = {
+	data: {
+		attributes: {
+			content: {
+				pid: number;
+				name: string;
+				typeText: string;
+				technicText: string;
+				layout: Layout;
+				images: { data: UploadFromStrapi[] };
+			}[];
+		};
+	};
+};
+
+export type { Realisation, StrapiPayload, Layout };
