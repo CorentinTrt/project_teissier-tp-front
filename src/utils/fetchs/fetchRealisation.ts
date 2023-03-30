95e42ae0ee8a80815ab52ab69c6e7424a88d4cab
CorentinTrt <corentintruffaut@gmail.com>
2023-03-30 07:26:18 -0400

    fetchs/fetchRealisation - create fetcher

---
+++ src/utils/fetchs/fetchRealisation.ts
@@ -0,0 +1,10 @@
+const strapiHost = process.env.STRAPI_HOST;
+const strapiPort = process.env.STRAPI_PORT;
+
+export default async function fetechRealisation(pid: string) {
+	const result = await fetch(
+		`http://${strapiHost}:${strapiPort}/api/data-realisation?populate[content][populate][0]=images&populate[content][filters][pid][$eq]=${pid}`
+	);
+
+	return await result.json();
+}
