fd9df19e9d4c93faf2a89e797117e734b99e3aeb
CorentinTrt <corentintruffaut@gmail.com>
2023-03-31 14:33:34 -0400

    utils/fetchs/fetchRealisation - fix typo on function name

--- src/utils/fetchs/fetchRealisation.ts
+++ src/utils/fetchs/fetchRealisation.ts
@@ -1,7 +1,7 @@
 const strapiHost = process.env.STRAPI_HOST;
 const strapiPort = process.env.STRAPI_PORT;
 
-export default async function fetechRealisation(pid: string) {
+export default async function fetchRealisation(pid: string) {
 	const result = await fetch(
 		`http://${strapiHost}:${strapiPort}/api/data-realisation?populate[content][populate][0]=images&populate[content][filters][pid][$eq]=${pid}`
 	);
