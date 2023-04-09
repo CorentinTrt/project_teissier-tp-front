7a11dcee386fd8bc8cf38a4ac5a7635aa4aea305
CorentinTrt <corentintruffaut@gmail.com>
2023-04-09 10:19:08 -0400

    pages/Contact - implement new props and types

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,25 +1,34 @@
+import ContactPage from '@pages/Contact';
+import Metas from '@sections/Metas';
 import Footer from '@blocs/Footer';
 import Header from '@blocs/Header';
-import { FooterData } from '@c_types/T_footerData';
-import ContactPage from '@pages/Contact';
 
 import construFooterData from '@utils/constructors/construFooterData';
+import construPageContact from '@utils/constructors/construPageContact';
 import fetchData from '@utils/fetchs/fetchData';
 import fetchPageData from '@utils/fetchs/fetchPageData';
 
+import { FooterData } from '@c_types/T_footerData';
+import { Metadata } from '@c_types/T_generics';
+import { Strings } from '@c_types/T_pageContactData';
+
 type Props = {
+	metadata: Metadata;
+	strings: Strings;
 	footerData: FooterData;
 };
 
 const Contact = (props: Props) => {
-	const { footerData } = props;
+	const { metadata, strings, footerData } = props;
 	const { details, contact } = footerData;
 
 	return (
 		<>
+			<Metas title={metadata.title} description={metadata.description} />
+
 			<Header />
 
-			<ContactPage details={details} contact={contact} />
+			<ContactPage strings={strings} details={details} contact={contact} />
 
 			<Footer />
 		</>
@@ -34,9 +43,12 @@ export async function getStaticProps() {
 	]);
 
 	const footerData = construFooterData(footer, contact);
+	const pageData = construPageContact(page);
 
 	return {
 		props: {
+			metadata: pageData.metadata,
+			strings: pageData.strings,
 			footerData: {
 				details: footerData.details,
 				contact: footerData.contact,
