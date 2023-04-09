adb4d2c5d320df09d8cc11a30cc448ea4eb1f472
CorentinTrt <corentintruffaut@gmail.com>
2023-04-09 09:56:28 -0400

    3_blocs/Header - add Page Contact feature

--- src/components/3_blocs/Header/index.tsx
+++ src/components/3_blocs/Header/index.tsx
@@ -1,9 +1,14 @@
+import { useRouter } from 'next/router';
+
 import s from './header.module.scss';
 
 import ActiveLink from '@shared/ActiveLink';
 import Button from '@shared/Button/Button';
 
 const Header = () => {
+	const router = useRouter();
+	const isOnPageContact = router.pathname === '/contact' ? true : false;
+
 	return (
 		<div className={s['header']}>
 			<ActiveLink href={'/'}>
@@ -13,7 +18,14 @@ const Header = () => {
 			<div className={s['right']}>
 				<a href={'#expertise'}>{'Expertise'}</a>
 				<a href={'#realisation'}>{'Réalisations'}</a>
-				<Button size='big' label='Contactez-nous' type='link' href='/contact' />
+				{!isOnPageContact && (
+					<Button
+						size='big'
+						label='Contactez-nous'
+						type='link'
+						href='/contact'
+					/>
+				)}
 			</div>
 		</div>
 	);
