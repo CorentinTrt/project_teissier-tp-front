e0e5a99380efc606fdecf062b66a90c9c3aadc85
CorentinTrt <corentintruffaut@gmail.com>
2023-04-09 09:53:43 -0400

    3_blocs/Footer - add Page Contact feature

--- src/components/3_blocs/Footer/index.tsx
+++ src/components/3_blocs/Footer/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import s from './footer.module.scss';
 
 import { Contact, FooterDetails } from '@c_types/T_footerData';
@@ -17,31 +19,46 @@ const Footer = (props: Props) => {
 	const { shotText, location } = details;
 	const { cellNumber, homeNumber, emailAdress } = contact;
 
+	const router = useRouter();
+	const isOnContactPage = router.pathname === '/contact' ? true : false;
+
 	return (
 		<div className={s['footer']}>
-			<div className={s['left']}>
-				<img src='/images/logo_blanc.png' alt='Logo Teissier TP' />
-				<p className={``}>{shotText}</p>
-
-				<div className={s['location']}>
-					<img src='/icons/location_white.png' alt='Icon location' />
-					<p>{location}</p>
+			{isOnContactPage && (
+				<div className={s['full']}>
+					<img src='/images/logo_blanc.png' alt='Logo Teissier TP' />
 				</div>
-			</div>
+			)}
 
-			<div className={s['right']}>
-				<h3>{'Contactez-nous'}</h3>
+			{!isOnContactPage && (
+				<>
+					<div className={s['left']}>
+						<img src='/images/logo_blanc.png' alt='Logo Teissier TP' />
+						<p className={``}>{shotText}</p>
 
-				<div className={s['numbers']}>
-					<img src='/icons/phone_white.png' alt='Icon phone' />
-					<p className={s['text']}>{`${cellNumber} | ${homeNumber}`}</p>
-				</div>
+						{!isOnContactPage && (
+							<div className={s['location']}>
+								<img src='/icons/location_white.png' alt='Icon location' />
+								<p>{location}</p>
+							</div>
+						)}
+					</div>
 
-				<div className={s['email-adress']}>
-					<img src='/icons/mail_white.png' alt='Icon email' />
-					<p>{emailAdress}</p>
-				</div>
-			</div>
+					<div className={s['right']}>
+						<h3>{'Contactez-nous'}</h3>
+
+						<div className={s['numbers']}>
+							<img src='/icons/phone_white.png' alt='Icon phone' />
+							<p className={s['text']}>{`${cellNumber} | ${homeNumber}`}</p>
+						</div>
+
+						<div className={s['email-adress']}>
+							<img src='/icons/mail_white.png' alt='Icon email' />
+							<p>{emailAdress}</p>
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
